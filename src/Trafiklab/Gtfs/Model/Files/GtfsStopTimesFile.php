44f3f27c66785aea0a52434c4c44c325eafa03a1
Bert <bert.marcelis@samtrafiken.se>
2019-05-23 13:03:27 +0200

    Better efficiency for retrieving stop_times by stop

--- src/Trafiklab/Gtfs/Model/Files/GtfsStopTimesFile.php
+++ src/Trafiklab/Gtfs/Model/Files/GtfsStopTimesFile.php
@@ -12,9 +12,20 @@ class GtfsStopTimesFile
 {
     private $dataRows;
 
+    private $stopTimesByStop = [];
+
     public function __construct(GtfsArchive $parent, string $filePath)
     {
         $this->dataRows = GtfsParserUtil::deserializeCSV($parent, $filePath, StopTime::class);
+
+        // Create an array which gives all stop_times for each stop.
+        // This way we only need to loop once over all stop times, but memory usage will be a bit higher.
+        foreach ($this->dataRows as $stopTime) {
+            if (!key_exists($stopTime->getStopId(), $this->stopTimesByStop)) {
+                $this->stopTimesByStop[$stopTime->getStopId()] = [];
+            }
+            $this->stopTimesByStop[$stopTime->getStopId()] = $stopTime;
+        }
     }
 
     /**
@@ -34,14 +45,12 @@ class GtfsStopTimesFile
      */
     public function getStopTimesForStop(string $stopId): array
     {
-        $stopTimes = $this->getStopTimes();
-        $result = [];
-        foreach ($stopTimes as $stopTime) {
-            if ($stopTime->getStopId() == $stopId) {
-                $result[] = $stopTime;
-            }
+        // Before we run a computationally intensive loop, check the table with included stops first.
+        if (!key_exists($stopId, $this->stopTimesByStop)) {
+            return [];
         }
-        return $result;
+
+        return $this->stopTimesByStop[$stopId];
     }
 
     /**
@@ -51,8 +60,8 @@ class GtfsStopTimesFile
      */
     public function getStopTimesForTrip(string $tripId): array
     {
-        $stopTimes = $this->getStopTimes();
         $result = [];
+        $stopTimes = $this->getStopTimes();
         foreach ($stopTimes as $stopTime) {
             if ($stopTime->getTripId() == $tripId) {
                 $result[] = $stopTime;
