ae191a65dff5d27916c06d9c125014eebe25d545
Bert Marcelis <bert.marcelis@samtrafiken.se>
2022-04-12 17:58:53 +0200

    Reduce test feed size, fix bug in shape lookup, fixes #6

--- src/Trafiklab/Gtfs/Util/Internal/BinarySearchUtil.php
+++ src/Trafiklab/Gtfs/Util/Internal/BinarySearchUtil.php
@@ -8,8 +8,6 @@ class BinarySearchUtil
 {
 
     /**
-     *
-     * /**
      * Find the index of the first ShapePoint for a given field value, by using binary search.
      *
      * @param $array     array The data to search through
@@ -23,23 +21,43 @@ class BinarySearchUtil
         return self::doBinarySearch($array, $getter, $value, 0, count($array));
     }
 
-    private static function doBinarySearch(array $array, string $getter, string $value, int $start, int $stop)
+    /**
+     * @param array  $array  Haystack
+     * @param string $getter Getter to get value from array item
+     * @param string $needle Needle to find
+     * @param int    $start  Inclusive start of range
+     * @param int    $stop   Exclusive end of range
+     *
+     * @return float|int
+     */
+    private static function doBinarySearch(array $array, string $getter, string $needle, int $start, int $stop)
     {
-        if ($start == $stop) {
+        if ($start + 1 == $stop) {
+            if ($array[$start] != $needle) {
+                return -1;
+            }
             return $start;
         }
-        $middle = $start + (($stop - $start) / 2);
 
-        if ($array[$middle]->$getter() > $value) {
-            return self::doBinarySearch($array, $getter, $value, $start, $middle);
+        $middle = $start + (($stop - $start) / 2);
+        $middleValue = $array[$middle]->$getter();
+
+        if ($middle != $start && $middle != $stop) {
+            if ($middleValue > $needle) {
+                return self::doBinarySearch($array, $getter, $needle, $start, $middle);
+            }
+
+            if ($middleValue < $needle) {
+                return self::doBinarySearch($array, $getter, $needle, $middle, $stop);
+            }
         }
 
-        if ($array[$middle]->$getter() < $value) {
-            return self::doBinarySearch($array, $getter, $value, $middle, $stop);
+        if ($middleValue != $needle) {
+            return -1;
         }
 
         // If the value equals, we just loop back to the first match
-        while ($middle > 0 && $array[$middle - 1]->$getter() == $value) {
+        while ($middle > 0 && $array[$middle - 1]->$getter() == $needle) {
             $middle--;
         }
         return $middle;
